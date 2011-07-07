4a0b9bdbd601c3578c769d784d9c416074d788e8
Colin Seymour <lildood@gmail.com>
2011-07-07 13:22:14 +0100

    Tidied up header

--- header.php
+++ header.php
@@ -8,34 +8,34 @@
 	<meta charset="utf-8">
 
 	<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
-	   Remove this if you use the .htaccess -->
+	   Remove this if you use the .htaccess 
+	   More info: h5bp.com/b/378 -->
 	<!--[if IE]>   
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<![endif]-->
-	
-	<title><?php echo $meta_title; ?></title>
-	<meta name="description" content="<?php echo $meta_description; ?>">
-	<meta name="author" content="Colin Seymour">
-	<meta name="keywords" content="<?php echo $meta_tags; ?>">
-	
+
+	<title><?php echo $theme->page_title; ?></title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
 	<!-- Mobile viewport optimized: j.mp/bplateviewport -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
+
 	<link rel="canonical" href="<?php URL::out(); ?>">
 	<link rel="shortcut icon" href="<?php Site::out_url( 'theme' ); ?>/img/favicon.ico">
 	<link rel="apple-touch-icon" href="<?php Site::out_url( 'theme' ); ?>/img/apple-touch-icon.png">
 
 	<!-- More ideas for your <head> here: h5bp.com/docs/#head-Tips -->
 
+	<!-- Load the stylesheet and your header Javascript -->
 	<?php $theme->header(); ?>
 </head>
 
 <body>
 
-  <div id="container">
-    <header>
-		<a name="top" id="top"></a>
-		<h1><a href="<?php Site::out_url( 'habari' ); ?>/"><?php Options::out( 'title' ); ?></a></h1>
-		<p><?php Options::out( 'tagline' ); ?></p>
-    </header>
-	<div id="main" role="main">
+	<div id="container">
+		<header>
+			<h1><a href="<?php Site::out_url( 'habari' ); ?>/"><?php Options::out( 'title' ); ?></a></h1>
+			<p><?php Options::out( 'tagline' ); ?></p>
+		</header>
+		<div id="main" role="main">
