cf4ae28e084ba054f770839a559006b7d0144eef
Colin Seymour <lildood@gmail.com>
2011-07-07 13:48:52 +0100

    Fixed <time> element content

--- home.php
+++ home.php
@@ -6,7 +6,7 @@ foreach ( $posts as $post ) : ?>
 				<header>
 					<h1><a href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>"><?php echo $post->title_out; ?></a></h1>
 					<div class="entry-meta">
-						<time datetime="<?php echo $post->pubdate_iso; ?>" pubdate="yes"><?php echo $post->pubdate->text_format('{d} {F} {Y}'); ?></time><span class="post_author"> by <a href="http://colinseymour.co.uk" target="_blank" rel="author"><?php echo $post->author->displayname; ?></a></span>
+						<time datetime="<?php echo $post->pubdate->text_format('{c}'); ?>" pubdate="yes"><?php $post->pubdate->out(); ?></time><span class="post_author"> by <?php echo $post->author->displayname; ?></span>
 						<span class="entry-tags">Tags: <?php echo ( count( $post->tags ) > 0 ) ? $post->tags_out : " - None - "; ?></span>
 						<span class="entry-commentslink"><a href="<?php echo $post->permalink; ?>#comments" title="Comments on this post'"><?php printf( '%d Comment', '%d Comments', $post->comments->approved->count, $post->comments->approved->count ); ?></a></span>
 					</div>
