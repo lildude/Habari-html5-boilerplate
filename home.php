263709f833dc464a81a9eadaef6cc117d1161174
Colin Seymour <lildood@gmail.com>
2011-07-07 15:18:53 +0100

    Fixed comment number display and updated section that displays the comment form

--- home.php
+++ home.php
@@ -8,18 +8,14 @@ foreach ( $posts as $post ) : ?>
 					<div class="entry-meta">
 						<time datetime="<?php echo $post->pubdate->text_format('{c}'); ?>" pubdate="yes"><?php $post->pubdate->out(); ?></time><span class="post_author"> by <?php echo $post->author->displayname; ?></span>
 						<span class="entry-tags">Tags: <?php echo ( count( $post->tags ) > 0 ) ? $post->tags_out : " - None - "; ?></span>
-						<span class="entry-commentslink"><a href="<?php echo $post->permalink; ?>#comments" title="Comments on this post'"><?php printf( '%d Comment', '%d Comments', $post->comments->approved->count, $post->comments->approved->count ); ?></a></span>
+						<span class="entry-commentslink"><a href="<?php echo $post->permalink; ?>#comments" title="Comments on this post'"><?php echo $post->comments->approved->count . _n( ' Comment', ' Comments', $post->comments->approved->count ); ?></a></span>
 					</div>
 				</header>
 				<div class="entry-content">
 					<?php echo $post->content_out; ?>
 				</div>
 			</article>
-		<?php if ( $request->display_entry === true ) : // Only display comments on posts ?>
-			<div class="entry-comments" id="comments">
-				<?php $theme->display( 'comments' ); ?>
-			</div>
-		<?php endif; ?>
+		<?php if ( $request->display_entry === true ) { $theme->display( 'comments' ); } // Display comment form if viewing a single entry ?>
 <?php endforeach; ?>
 		
 		</div> <!-- close #main -->
